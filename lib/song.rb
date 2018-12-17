890fb2067e73797074e924ad256c7dbfb00bc32d
Milena Kari <milenakari@yahoo.com>
2018-12-17 01:04:33 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name=name
+  end
+
+  def new_by_filename(filename)
+    name_array = filename.split(" - ")
+    song = self.new(name_array[1])
+    song.artist.name = name_array[0]
+    song
+  end
+end
