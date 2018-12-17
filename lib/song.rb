a1c8d39251f88aa882f0432b78eff7f65d41806e
Milena Kari <milenakari@yahoo.com>
2018-12-17 01:11:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def self.new_by_filename(filename)
     name_array = filename.split(" - ")
     song = self.new(name_array[1])
-    song.artist.name = name_array[0]
+    #song.artist.name = name_array[0]
     song
   end
 end
